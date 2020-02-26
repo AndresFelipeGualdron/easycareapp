aa1225802959d5ce96a375b9144239ca80879ae3
AndresFelipeGualdron <gualdronsito@hotmail.com>
2020-02-26 11:40:13 -0800

    cambiando imagen principal

---
+++ src/components/holaMundoComponent/holaMundo.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class HolaMundo extends Component{
+
+    render(){
+        return (
+            <div>
+                <img alt="imagen principal" src="https://raw.githubusercontent.com/AndresFelipeGualdron/easyCare/master/mockups/1.png"/>
+            </div>
+        );
+    }
+
+}
