d1d455c6e555e66d12ec4f15e033546ba42bbd35
AndresFelipeGualdron <gualdronsito@hotmail.com>
2020-02-28 15:46:42 -0500

    agregando bootstrap y jquery

--- src/components/holaMundoComponent/holaMundo.js
+++ src/components/holaMundoComponent/holaMundo.js
@@ -2,11 +2,20 @@ import React, {Component} from 'react';
 
 export default class HolaMundo extends Component{
 
+    
+
     render(){
+
+        function saludar() {
+            console.log("hola a todos");
+        }
+
         return (
             <div>
                 <img alt="imagen principal" src="https://raw.githubusercontent.com/AndresFelipeGualdron/easyCare/master/mockups/1.png"/>
+                <button className="square" onClick={saludar()} type="button">Saluda.</button>
             </div>
+            
         );
     }
 
