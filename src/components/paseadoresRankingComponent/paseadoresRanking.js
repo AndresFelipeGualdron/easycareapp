4f87b59b00dac3269603c075ac44042c7f293a5e
AndresFelipeGualdron <gualdronsito@hotmail.com>
2020-03-20 10:31:10 -0500

    adelantando el ranking de paseadores

--- src/components/paseadoresRankingComponent/paseadoresRanking.js
+++ src/components/paseadoresRankingComponent/paseadoresRanking.js
@@ -4,12 +4,64 @@ import Header from '../headerComponent/header';
 
 export default class PaseadoresRanking extends Component{
 
+    constructor(){
+        super();
+        this.state = {
+            paseadores : []
+        }
+
+    }
+
+    //PEDIR PASEADORES
+
+    obtenerPaseadores = function(){
+
+    }
+
+    obtenerPaseadoresCorrecto = function(data){
+
+    }
+
+    obtenerPaseadoresIncorrecto = function(error){
+
+    }
+
+    //FIN PEDIR PASEADORES
+
 
 
     render(){
         return (
             <React.Fragment>
                 <Header/>
+                <div className="container">
+                    <table className="table table-hover">
+                        <thead>
+                        <tr>
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>Email</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>John</td>
+                            <td>Doe</td>
+                            <td>john@example.com</td>
+                        </tr>
+                        <tr>
+                            <td>Mary</td>
+                            <td>Moe</td>
+                            <td>mary@example.com</td>
+                        </tr>
+                        <tr>
+                            <td>July</td>
+                            <td>Dooley</td>
+                            <td>july@example.com</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
             </React.Fragment>
         );
     }
