60d04abeb6ea17dc3e1b32bee72cfcf7bd85d951
migue1994 <migue24co@gmail.com>
2020-05-20 15:58:59 -0500

    Rollback

--- src/components/homeComponent/home.js
+++ src/components/homeComponent/home.js
@@ -12,7 +12,7 @@ export default class Home extends Component {
             <div className="container-fluid">
                 <Header
                 />
-                <Prueba
+                <Bienvenida
                 />
             </div>
         );
